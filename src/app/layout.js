89bf407b4b2992d2dcd930a2a739f41e957dfd37
Vishal <vishal.bhumca21@gmail.com>
2025-08-13 23:56:37 +0530

    depenedices added font family added bare minimum folder structure

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,14 +1,20 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Poppins,Roboto,Roboto_Mono } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const poppins = Poppins({
   subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-poppins",
 });
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const roboto = Roboto({
   subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-roboto",
+});
+const robotoMono = Roboto_Mono({
+  subsets: ["latin"],   
+  weight: ["100", "200", "300", "400", "500", "600", "700", ],
+  variable: "--font-roboto-mono",
 });
 
 export const metadata = {
@@ -20,7 +26,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${poppins.variable} ${roboto.variable} ${robotoMono.variable} antialiased`}
       >
         {children}
       </body>
